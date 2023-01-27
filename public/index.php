bb1b64528597bef3354dbdb6b4e294062e182c90
miltonpaiva <miltonpaiva268@gmail.com>
2023-01-27 15:32:41 -0300

    refactor: teste de conexão

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-<?php echo "fuuuuckkkk"; ?>
+<?php 
+
+$servername = "localhost";
+$username = "miltonpaiva";
+$password = "senha";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+?>
